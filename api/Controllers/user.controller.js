520095c5770096bda9fb1eab0fe617866d58d49c
VSat08 <satyam2001verma@gmail.com>
2024-05-02 12:18:11 +0530

    Createded test API Routes

---
+++ api/Controllers/user.controller.js
@@ -0,0 +1,5 @@
+export const test  = (req, res) => {
+  res.json({
+    message: "Testing USER Route",
+  });
+};
--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+import userRouter from "./routes/user.route.js"
+
 dotenv.config();
 
 mongoose
@@ -16,3 +18,7 @@ mongoose
 const app = express();
 
 app.listen(3000, () => console.log("> Server is up and running on 3000 : "));
+
+app.use("/api/user", userRouter);
+
+
