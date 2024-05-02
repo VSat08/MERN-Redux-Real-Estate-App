802c6f8a56ea3aae99975d28d091d38690d28384
VSat08 <satyam2001verma@gmail.com>
2024-05-02 11:44:10 +0530

    Database Connected

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.DATABASE)
+  .then(() => {
+    console.log("Databse connected successfully!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 const app = express();
 
 app.listen(3000, () => console.log("> Server is up and running on 3000 : "));
