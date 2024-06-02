0d0527717b49ac4fcb32f12021ce63e41196d540
VSat08 <satyam2001verma@gmail.com>
2024-06-02 15:19:26 +0530

    Revised: Added Delete User Functionality

--- api/Controllers/user.controller.js
+++ api/Controllers/user.controller.js
@@ -46,7 +46,7 @@ export const deleteUser = async (req, res, next) => {
     await User.findByIdAndDelete(req.params.id);
 
     res.clearCookie("access_token");
-    res.status(200).json("User has been deleted!").clearCookie("access_token");
+    res.status(200).json("User has been deleted!");
   } catch (error) {
     return next(error);
   }
