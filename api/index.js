64c6c8694bdd37bbce249e0803cc08d60ecf3966
VSat08 <satyam2001verma@gmail.com>
2024-06-04 12:31:34 +0530

    Added Create Listing API Route!

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 
 dotenv.config();
 
@@ -25,6 +26,7 @@ app.listen(3000, () => console.log("> Server is up and running on 3000 : "));
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 // middleware routes for errro handling
 
