5055bc15fe84d2fc08f4ee6cc55c87c508e54f6e
VSat08 <satyam2001verma@gmail.com>
2024-06-23 23:40:16 +0530

    Created get User Lisitngs  API route!!

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   deleteUser,
+  getUserLisitng,
   test,
   updateUser,
 } from "../controllers/user.controller.js";
@@ -11,5 +12,6 @@ const router = express.Router();
 router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listing/:id", verifyToken, getUserLisitng);
 
 export default router;
