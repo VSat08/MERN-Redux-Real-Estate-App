81b4cb98c21c1c52b53cb67cce39f324c48cbcf2
VSat08 <satyam2001verma@gmail.com>
2024-06-26 14:12:03 +0530

    Created Update Listing API Route !

--- api/Controllers/listing.controller.js
+++ api/Controllers/listing.controller.js
@@ -26,3 +26,25 @@ export const deleteListing = async (req, res, next) => {
     return next(error);
   }
 };
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found"));
+  }
+
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(404, "You can only edit your own listings!!"));
+  }
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedListing);
+  } catch (error) {
+    return next(error);
+  }
+};
