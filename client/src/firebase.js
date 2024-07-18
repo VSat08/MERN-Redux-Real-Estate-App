ba2c10c00bc5c1d16664937a4ff346d1b64dc2f9
VSat08 <satyam2001verma@gmail.com>
2024-07-18 21:48:11 +0530

    Pre Deployment

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,11 +2,11 @@ import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_FIREBASE_APPID,
+  authDomain: "mern-redux-real-estate.firebaseapp.com",
+  projectId: "mern-redux-real-estate",
+  storageBucket: "mern-redux-real-estate.appspot.com",
+  messagingSenderId: "446710068727",
+  appId: "1:446710068727:web:b9a4c90bc9a076a378aaf2",
 };
 
 export const app = initializeApp(firebaseConfig);
